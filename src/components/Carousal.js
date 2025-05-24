26a361628b5ddfde54f438604b0e378979d0070c
AdarshVasudev07 <adarshvasudev8055@gmail.com>
2025-05-23 21:04:04 -0400

    first

---
+++ src/components/Carousal.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Carousal() {
+  return (
+    <div><div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://images.unsplash.com/photo-1571091718767-18b5b1457add?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="https://images.unsplash.com/photo-1571091718767-18b5b1457add?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="hhttps://images.unsplash.com/photo-1509722747041-616f39b57569?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div></div>
+  )
+}
---
+++ src/screens/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Card from '../components/Card'
+import Carousal from '../components/Carousal'
+
+export default function Home() {
+  return (
+    <div>
+      <div> <Navbar></Navbar></div>
+      <div><Carousal></Carousal></div>
+
+      <div>
+        <Card></Card>
+      </div>
+
+      <div> <Footer></Footer></div>
+
+    </div>
+  )
+}
